e65f52c8f7d57622eaa41e6891ac6af04f19507a
Eronar Alves <alveseronar@gmail.com>
2023-07-31 14:57:39 -0300

    feat: list exercises history

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,50 @@
-import { VStack } from 'native-base'
+import React, { useState } from 'react'
+import { Heading, VStack, SectionList, Text } from 'native-base'
 
 // Components
 import { ScreenHeader } from '@components/ScreenHeader'
+import { HistoryCard } from '@components/HistoryCard'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.22",
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: "27.08.22",
+      data: ['Puxada frontal']
+    },
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        contentContainerStyle={exercises.length === 0 && {
+          flex: 1, justifyContent: 'center'
+        }}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há registrados ainda. {'\n'}
+            Vamos treinar hoje?
+          </Text>
+        )}
+        px={8}
+      />
+
     </VStack>
   )
 }
