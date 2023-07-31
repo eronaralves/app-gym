fc45d00818e05e9a55b6de45d204ef9f508f7f4e
Eronar Alves <alveseronar@gmail.com>
2023-07-31 14:21:07 -0300

    feat: component exercise card

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
-import { FlatList, HStack, VStack } from 'native-base'
+import { FlatList, HStack, VStack, Heading, Text } from 'native-base'
 
 // Components
-import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra',])
   const [groups, setGroups] = useState(['costas', 'Bíceps', 'Tríceps', 'ombro',])
   const [groupSelected, setGroupSelected] = useState('costas')
 
@@ -31,6 +33,29 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack mb={5} alignItems="center" justifyContent="space-between">
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20
+          }}
+        />
+      </VStack>
     </VStack>
   )
 }
