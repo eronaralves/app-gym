439eefedbf4ddd6d5b059dd4e444bddb1016d194
Eronar Alves <alveseronar@gmail.com>
2023-07-31 15:50:56 -0300

    feat: form profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,23 +1,87 @@
-import { Center, ScrollView, VStack } from 'native-base'
+import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base'
 
 // Components
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{
-              uri: 'https://www.github.com/eronaralves.png'
-            }} 
-            alt="Imagem do usuario"
-            size={33}
-          />
+          {
+            photoIsLoading ?
+              <Skeleton 
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              />
+              : 
+              <UserPhoto
+                source={{
+                  uri: 'https://www.github.com/eronaralves.png'
+                }} 
+                alt="Imagem do usuario"
+                size={PHOTO_SIZE}
+              />         
+            }
+            <TouchableOpacity>
+              <Text color="green.500" fontWeight="bold" fontSize="md" mb={8} mt={2}>
+                Alterar foto
+              </Text>
+            </TouchableOpacity>
+
+            <Input 
+              bg="gray.600"
+              placeholder='Nome'
+            />
+
+            <Input 
+              bg="gray.600"
+              placeholder='Email'
+              isDisabled
+            />
         </Center>
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+          <Input 
+            bg="gray.600"
+            placeholder='Senha antiga'
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder='Nova senha'
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder='Confirme a nova senha'
+            secureTextEntry
+          />
+
+          <Button 
+            title='Atualizar' 
+            mt={4}
+          />
+        </VStack>
       </ScrollView>
     </VStack>
   )
