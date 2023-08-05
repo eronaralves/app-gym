9462c30657cc8b84dc1b9450d9339c959501f32c
Eronar Alves <alveseronar@gmail.com>
2023-08-05 18:55:52 -0300

    feat: screnn exercise

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,17 @@
 import { TouchableOpacity } from 'react-native'
-import { VStack, Icon, HStack, Heading, Text } from 'native-base'
+import { VStack, Icon, HStack, Heading, Text, ScrollView, Image, Box } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons'
 
 // Images
 import BodySvg from '@assets/body.svg'
+import SeriesSvg from '@assets/series.svg'
+import RepetitionsSvg from '@assets/repetitions.svg'
+
+// Components
+import { Button } from '@components/Button'
+
+
 
 export function Exercise() {
   const navigation = useNavigation()
@@ -39,6 +46,45 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
+
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            w="full"
+            h={80}
+            source={{
+              uri: "https://images.tcdn.com.br/img/img_prod/1169104/pulley_fokus_com_remada_4942_2_cb1eb92bf61a865aadc1e180a808294f.jpg",
+            }}
+            alt="Nome do exercicio"
+            mb={3}
+            resizeMode="cover"
+            rounded="lg"
+          />
+
+          <Box bg="gray.600" rounded="md" pb={4} px={4}>
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb={6}
+              mt={5}
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="gray.200" ml={2}>
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="gray.200" ml={2}>
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
